2214b13475517aac3ebd0be9bbb0e3c7cfc2a8ec
Simone Bliss <sbliss5@jhu.edu>
2021-12-10 18:05:12 -0500

    running exec

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	//"math/big"
 	//"log"
-	//"os"
+	"os"
+	"os/exec"
 	//"github.com/alichator/wg-lite/noise"
 )
 
@@ -12,5 +13,34 @@ func main() {
 	fmt.Println("Simone")
 	fmt.Println("oop")
 	//log.SetFlags(0)
+	wgpath := os.Args[1]
+	fmt.Println(wgpath)
+	var inmess1 = "inmess1.txt"
+	var inmess2 = "inmess2.txt"
+	var outmess = "outmess.txt"
+
+	// Get path for cwebp program.
+	//exe, _ := exec.LookPath(wgpath)
+
+	var program = wgpath + "/main.go"
+	fmt.Println(program)
+
+	// Build up argument string with slice.
+	arguments := []string{}
+	arguments = append(arguments, "client")
+	arguments = append(arguments, "1")     //seed
+	arguments = append(arguments, "1")     //step
+	arguments = append(arguments, outmess) //step
+	arguments = append(arguments, inmess1) //step
+	arguments = append(arguments, inmess2) //step
+
+	// Call exec.Command with arguments string.
+	build := exec.Command("go", "install")
+	build.Dir = wgpath
+	e := build.Run()
+	fmt.Println(e)
+	build = exec.Command("wg-lite", arguments...)
+	e = build.Run()
+	fmt.Println(e)
 
 }
