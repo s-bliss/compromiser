f8f7c70858a8d43d5791ff23204fc4d2f4ddb4c9
Simone Bliss <sbliss5@jhu.edu>
2021-12-10 14:56:27 -0500

    setup ok

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/s-bliss/compromiser
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	//"math/big"
+	//"log"
+	//"os"
+	//"github.com/alichator/wg-lite/noise"
+)
+
+func main() {
+	fmt.Println("Simone")
+	fmt.Println("oop")
+	//log.SetFlags(0)
+
+}
